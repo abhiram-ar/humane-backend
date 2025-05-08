44ba80e726cf8306d3cf8c045b6b23b740868aa6
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-08 11:40:34 +0530

    notification server configues

--- notification-service/src/adapter/in/http/server.ts
+++ notification-service/src/adapter/in/http/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
 
 app.get('/api/v1/notification/health', (req, res) => {
+   console.log('hello from nofi');
    res.status(200).json({ status: 'OK' });
 });
 
