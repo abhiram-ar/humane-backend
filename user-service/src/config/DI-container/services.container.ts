34e52863e3391caa894941d3c78d719baaddf965
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-10 12:21:32 +0530

    added kafka publishr to dep

--- user-service/src/config/DI-container/services.container.ts
+++ user-service/src/config/DI-container/services.container.ts
@@ -1,4 +1,6 @@
 import { OTP } from '@domain/services/otpGenerator';
+import { KafkaPublisher } from '@infrastructure/eventBus/KafkaPublisher';
+import KafkaSingleton from '@infrastructure/eventBus/KafkaSingleton';
 import { BcryptHashService } from '@infrastructure/service/BcryptHashService';
 import { CryptoUUIDService } from '@infrastructure/service/CryptoUUIDService';
 import { JWTService } from '@infrastructure/service/JWTService';
@@ -8,3 +10,6 @@ const bcryptHashService = new BcryptHashService();
 const otpService = new OTP();
 const cryptoUUIDService = new CryptoUUIDService();
 export { jwtService, bcryptHashService, otpService, cryptoUUIDService };
+
+
+export const kafkaPubliserService = new KafkaPublisher(KafkaSingleton.getInstance())
