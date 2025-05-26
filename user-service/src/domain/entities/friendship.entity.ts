f86acc37912308ecb5711f1ec6a7f9ec87c1e216
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-26 13:44:59 +0530

    updaed entritty

--- user-service/src/domain/entities/friendship.entity.ts
+++ user-service/src/domain/entities/friendship.entity.ts
@@ -1,7 +1,15 @@
-type FriendshipStatus = 'PENDING' | 'ACCEPTED' | 'DECLINED' | 'STRANGERS';
+type FriendshipStatus = 'PENDING' | 'ACCEPTED' | 'DECLINED';
 
 export class Friendship {
-   constructor(public userId1: string, public userId2: string, public status: FriendshipStatus) {
+   constructor(
+      public userId1: string,
+      public userId2: string,
+      public status: FriendshipStatus,
+      public requesterId?: string,
+      public recieverId?: string,
+      public createdAt?: string,
+      public updatedAt?: string
+   ) {
       if (userId1 === userId2) throw new Error('UserIds must be different');
       if (userId1 > userId2) {
          [this.userId2, this.userId1] = [userId1, userId2];
