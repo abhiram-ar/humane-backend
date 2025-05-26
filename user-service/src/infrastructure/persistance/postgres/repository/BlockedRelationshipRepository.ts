2d379ad5650a7cb230f978ed2ca1180990718c3a
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-26 18:20:44 +0530

    better semanitcs

--- user-service/src/infrastructure/persistance/postgres/repository/BlockedRelationshipRepository.ts
+++ user-service/src/infrastructure/persistance/postgres/repository/BlockedRelationshipRepository.ts
@@ -1,7 +1,12 @@
 import { IBlockedRelationshipRepository } from '@ports/IBlockedRelationshipRepository';
+import db from '../prisma-client';
 
 export class PostgresBlockedRelationshipRepository implements IBlockedRelationshipRepository {
-   isBlockedBy(checkUserId: string, blockerId: string): Promise<Boolean> {
-      throw new Error('Method not implemented.');
-   }
+   isBlockedBy = async (checkUserId: string, blockerId: string): Promise<Boolean> => {
+      const res = await db.blockedUser.findUnique({
+         where: { blockerId_blockedId: { blockerId, blockedId: checkUserId } },
+      });
+
+      return !!res;
+   };
 }
