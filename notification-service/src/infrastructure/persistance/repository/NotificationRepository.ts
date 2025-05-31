02d3c66c4a992456c6242f63271d719d9d809628
abhiram-ar <abhiram2001.ars@gmail.com>
2025-05-31 18:31:25 +0530

    repo

--- notification-service/src/infrastructure/persistance/repository/NotificationRepository.ts
+++ notification-service/src/infrastructure/persistance/repository/NotificationRepository.ts
@@ -1,20 +1,5 @@
-import {
-   CombinedNotificationType,
-   CombinedNotification,
-} from '@domain/entities/CombinedNotification';
 import { INotificationRepository } from '@domain/interfaces/repository/INotificationRepository';
 
 export class MongoNotificationRepository implements INotificationRepository {
    constructor() {}
-   upsert<T extends CombinedNotificationType>(
-      notification: Omit<CombinedNotification, 'id'>
-   ): Promise<Extract<CombinedNotification, { type: T }>> {
-      throw new Error('Method not implemented.');
-   }
-   getUserNotifications(reciverId: string): Promise<CombinedNotification[] | null> {
-      throw new Error('Method not implemented.');
-   }
-   save(notication: CombinedNotification): Promise<CombinedNotification> {
-      throw new Error('Method not implemented.');
-   }
 }
