88cda381e5307d4ecc1694c1408d34520492c895
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-03 14:32:11 +0530

    cors

--- notification-service/src/presentation/http/app.ts
+++ notification-service/src/presentation/http/app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+   cors({
+      origin: ['http://localhost:5173', 'http://humanedevfe.abhiram-ar.com:5173'],
+      credentials: true,
+   })
+);
 
 app.get('/api/v1/notification/health', (req, res) => {
    res.status(200).json({ health: 'OK' });
