9b936f9ec27c0c313449ef01046edf40e4f13e8f
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-06 18:37:21 +0530

    basic post

--- writer-service/src/domain/repository/BaseRepository.ts
+++ writer-service/src/domain/repository/BaseRepository.ts
@@ -1,4 +1,7 @@
 export interface IBaseRepository<T> {
    create(entity: T): Promise<Required<T>>;
    delete(authorId: string, entityId: string): Promise<Required<T> | null>;
+
+   get(entiryId: string): Promise<Required<T> | null>;
+   exists(entityId: string): Promise<boolean>;
 }
--- writer-service/src/domain/repository/IPostRepository.ts
+++ writer-service/src/domain/repository/IPostRepository.ts
@@ -3,5 +3,6 @@ import { IBaseRepository } from './BaseRepository';
 
 export interface IPostRepository extends IBaseRepository<Post> {
    create(entity: Post): Promise<Required<Post>>;
-   delete(authorId: string, entityId: string): Promise<Required<Post> | null>;
+   delete(authorId: string, postId: string): Promise<Required<Post> | null>;
+   exists(postId: string): Promise<boolean>;
 }
