ccdeed71cf6100c89f2f429f610b155e8f4cb86e
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-09 14:36:25 +0530

    added comments'

--- writer-service/src/infrastructure/persistance/MongoDB/Models/commentModel.ts
+++ writer-service/src/infrastructure/persistance/MongoDB/Models/commentModel.ts
@@ -5,11 +5,14 @@ export interface ICommentDocument extends Required<Omit<Comment, 'id' | 'postId'
    postId: mongoose.Schema.Types.ObjectId;
 }
 
-const commentSchema = new mongoose.Schema<ICommentDocument>({
-   authorId: { type: String, required: true },
-   content: { type: String, required: true },
-   postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-});
+const commentSchema = new mongoose.Schema<ICommentDocument>(
+   {
+      authorId: { type: String, required: true },
+      content: { type: String, required: true },
+      postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+   },
+   { timestamps: true }
+);
 commentSchema.index({ authorId: 1, _id: -1 });
 
 const commentModel = mongoose.model<ICommentDocument>('Comment', commentSchema);
