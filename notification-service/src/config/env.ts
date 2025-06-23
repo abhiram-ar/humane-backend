4d8aa908fa13e98ff3ec8d7105be28958145bdd0
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-23 09:32:57 +0530

    monogDB uri from env

--- notification-service/src/config/env.ts
+++ notification-service/src/config/env.ts
@@ -1,26 +1,17 @@
-type ENVValue = string | undefined;
-
-type ENVConfig = {
-   readonly NODE_ENV: 'production' | 'development';
-   readonly SERVER_PORT: ENVValue;
-   readonly ACCESS_TOKEN_SECRET: ENVValue;
-   readonly KAFKA_CLIENT_ID: ENVValue;
-   readonly KAFKA_BROKER_URI: ENVValue;
-};
-
-export const ENV: ENVConfig = {
+export const ENV = {
    NODE_ENV: process.env.NODE_ENV as 'production' | 'development',
    SERVER_PORT: '3000',
    ACCESS_TOKEN_SECRET: process.env.accessTokenSecret,
    KAFKA_CLIENT_ID: process.env.KAFKA_CLIENT_ID,
    KAFKA_BROKER_URI: process.env.KAFKA_BROKER_URI,
-};
+   MONGODB_URI: process.env.MONGODB_URI,
+} as const;
 
 function checkEnv() {
    let errorCount = 0;
 
    for (let key in ENV) {
-      let typedKey = key as keyof ENVConfig;
+      let typedKey = key as keyof typeof ENV;
       if (ENV[typedKey] === undefined || ENV[typedKey] === null) {
          console.log(`${key} not found in environment`);
          errorCount++;
