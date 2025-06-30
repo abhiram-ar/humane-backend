13a042ba65f36f47b2096f7b078cf2740a3cc6cf
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-30 19:17:04 +0530

    type fix

--- elasticsearch-proxy/src/interfaces/dto/post/Post.dto.ts
+++ elasticsearch-proxy/src/interfaces/dto/post/Post.dto.ts
@@ -14,12 +14,14 @@ export const postSchema = z.object({
    visibility: z.enum([PostVisibility.FRIENDS, PostVisibility.PUBLIC]),
    hashtags: z.array(z.string()),
 
-   attachmentType: z.enum([PostAttachmentType.PHOTO, PostAttachmentType.VIDEO]),
-   attachmentStatus: z.enum([
-      PostAttachmentStatus.READY,
-      PostAttachmentStatus.PROCESSING,
-      PostAttachmentStatus.ERROR,
-   ]),
+   attachmentType: z.enum([PostAttachmentType.PHOTO, PostAttachmentType.VIDEO]).optional(),
+   attachmentStatus: z
+      .enum([
+         PostAttachmentStatus.READY,
+         PostAttachmentStatus.PROCESSING,
+         PostAttachmentStatus.ERROR,
+      ])
+      .optional(),
    rawAttachmentKey: z.string().nullish().optional(),
    processedAttachmentKey: z.string().nullish().optional(),
 
