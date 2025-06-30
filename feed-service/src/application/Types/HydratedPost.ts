efbcf525aedf2f6fcc72263375b9ba51e002ea84
abhiram-ar <abhiram2001.ars@gmail.com>
2025-06-30 18:17:49 +0530

    new post vlaidator

--- feed-service/src/application/Types/HydratedPost.ts
+++ feed-service/src/application/Types/HydratedPost.ts
@@ -1,15 +1,33 @@
-import { ModerationStatus, PostVisibility } from 'humane-common';
+import {
+   ModerationStatus,
+   PostAttachmentStatus,
+   PostAttachmentType,
+   PostVisibility,
+} from 'humane-common';
 import { UserBasicDetails } from './UserBasicDetails';
 
 export type HydratedPost = {
    author: UserBasicDetails | undefined;
+
    id: string;
-   createdAt: Date;
-   updatedAt: Date;
    authorId: string;
    content: string;
    visibility: (typeof PostVisibility)[keyof typeof PostVisibility];
-   moderationStatus: (typeof ModerationStatus)[keyof typeof ModerationStatus];
+   hashtags: string[];
+
+   attachmentType?: (typeof PostAttachmentType)[keyof typeof PostAttachmentType];
+   rawAttachmentKey?: string | null;
+   attachmentStatus?: (typeof PostAttachmentStatus)[keyof typeof PostAttachmentStatus];
+   rawAttachmentURL?: string | null;
+
+   moderationStatus: (typeof ModerationStatus)[keyof typeof ModerationStatus] | undefined; //TOTO: remove undfiend when mooderation service is implmented
    moderationMetadata?: any;
-   posterURL: string | null;
+
+   createdAt: Date;
+   updatedAt: Date;
+
+   // additionally added
+   commentCount: Number | null;
 };
+
+export type IPostDocument = {};
