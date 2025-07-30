6c877e252be62578fdb9ccb5cee5526c17616829
abhiram-ar <abhiram2001.ars@gmail.com>
2025-07-30 20:55:41 +0530

    updated event 24 hr first message type

--- chat-service/src/application/usecases/RepliedWithin24Hrs.usecase.ts
+++ chat-service/src/application/usecases/RepliedWithin24Hrs.usecase.ts
@@ -3,6 +3,7 @@ import { logger } from '@config/logger';
 import { IMessageRepository } from '@ports/repository/IMessageRepository';
 import { IEventPublisher } from '@ports/services/IEventProducer';
 import { AppEventsTypes, createEvent, MessageBrokerTopics } from 'humane-common';
+import { FirstReplyWithin24HrEventPayload } from 'humane-common/build/events/chat-service-events';
 
 export class RepliedWithin24Hrs {
    constructor(
@@ -10,16 +11,16 @@ export class RepliedWithin24Hrs {
       private readonly _eventPublisher: IEventPublisher
    ) {}
 
-   execute = async (dto: RepliedWithin24HrsInputDTO): Promise<void> => {
+   execute = async (userMsg: RepliedWithin24HrsInputDTO): Promise<void> => {
       // check if user already rewared in last 24hr in cache - if yes return
 
       // retrive the message send by other user in last 24hr - if no messag exist return
       const otherUserLastMessage =
          await this._messageRepo.getLastMessageOfOtherUserBeforeThisMessage({
-            messageId: dto.messageId,
-            convoId: dto.conversationId,
-            sendAt: dto.sendAt,
-            senderId: dto.senderId,
+            messageId: userMsg.messageId,
+            convoId: userMsg.conversationId,
+            sendAt: userMsg.sendAt,
+            senderId: userMsg.senderId,
          });
       if (!otherUserLastMessage) {
          logger.debug(`${RepliedWithin24Hrs.name}: no other user last message`);
@@ -27,17 +28,24 @@ export class RepliedWithin24Hrs {
          return;
       }
 
-      const sendTimeDelta = dto.sendAt.getTime() - otherUserLastMessage.sendAt.getTime();
+      const sendTimeDelta = userMsg.sendAt.getTime() - otherUserLastMessage.sendAt.getTime();
       // 24hrs
       if (sendTimeDelta > 86400000) {
          logger.debug(`${RepliedWithin24Hrs.name}: other user last message is older than 24hrs`);
          return;
       }
 
-      // publish rewarable event,
+      const firstReplyWithin24HrEvent: FirstReplyWithin24HrEventPayload = {
+         messageId: userMsg.messageId,
+         senderId: userMsg.senderId,
+         conversationId: userMsg.conversationId,
+         sendAt: userMsg.sendAt,
+         repliedToUserId: otherUserLastMessage.senderId,
+         message: userMsg.message,
+      };
       const firstReplyWithin24HrEventPayload = createEvent(
          AppEventsTypes.FIRST_REPLY_WITHIN_24_HR,
-         dto
+         firstReplyWithin24HrEvent
       );
 
       const { ack } = await this._eventPublisher.send(
