a7208dd9c37a4fce2e0db13e235ea138dbf27545
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-14 17:29:54 +0530

    added moderation-service to k8

--- moderation-service/src/main.ts
+++ moderation-service/src/main.ts
@@ -11,16 +11,16 @@ const bootstrap = async () => {
       await nsfwImageClassifierService.loadModel({ modelPath, imageSize: 299 });
 
       logger.info('starting job');
-      let res = await moderationService.execute({
-         attachmentKey: 'moderation0-test/tauta-tauba-song.mp4',
-         attachmentType: 'video/mp4',
-         bucketName: ENV.AWS_S3_BUCKET_NAME as string,
-         hotClassNames: ['Porn', 'Hentai'],
-         cleanup: true,
-      });
-      console.log(res);
-      if (res.success && res.moderdationData.type === 'multiFrame')
-         console.log(res.moderdationData.result.hotFrames.length);
+      // let res = await moderationService.execute({
+      //    attachmentKey: 'moderation0-test/tauta-tauba-song.mp4',
+      //    attachmentType: 'video/mp4',
+      //    bucketName: ENV.AWS_S3_BUCKET_NAME as string,
+      //    hotClassNames: ['Porn', 'Hentai'],
+      //    cleanup: true,
+      // });
+      // console.log(res);
+      // if (res.success && res.moderdationData.type === 'multiFrame')
+      //    console.log(res.moderdationData.result.hotFrames.length);
    } catch (error) {
       logger.error('Error while starting moderation service');
       console.log(error);
