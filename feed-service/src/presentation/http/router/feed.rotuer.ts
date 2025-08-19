3daf9c852fabe1d1e7ec3e9dd63b0fe746091c9c
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-19 20:06:32 +0530

    migrated to no autheication  v2

--- feed-service/src/presentation/http/router/feed.rotuer.ts
+++ feed-service/src/presentation/http/router/feed.rotuer.ts
@@ -1,8 +1,8 @@
 import { timelineController } from '@di/controller.container';
 import express from 'express';
-import { isAuthenticated } from 'humane-common';
+import { isAuthenticatedV2 } from 'humane-common';
 
 const feedRouter = express.Router();
 
-feedRouter.get('/', isAuthenticated, timelineController.getTimeline);
+feedRouter.get('/', isAuthenticatedV2, timelineController.getTimeline);
 export default feedRouter;
