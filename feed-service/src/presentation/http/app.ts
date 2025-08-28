b3fa0789f19480eaa4ee002535fc52deb41dc02c
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-28 14:12:07 +0530

    monitoring

--- feed-service/src/presentation/http/app.ts
+++ feed-service/src/presentation/http/app.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, {Response} from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieParse from 'cookie-parser';
-import { errorHandler } from 'humane-common';
+import { errorHandler, UnifiedPrometheusMetricsMonitoring } from 'humane-common';
 import feedRouter from './router/feed.rotuer';
+import * as promClient from 'prom-client';
+import { logger } from '@config/logger';
 
 const app = express();
 
@@ -13,9 +15,24 @@ app.use(
       credentials: true,
    })
 );
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
+const monitoring = new UnifiedPrometheusMetricsMonitoring(promClient);
+
+register.registerMetric(monitoring.httpRequestTotal);
+register.registerMetric(monitoring.httpRequestDuration);
+
+app.use(monitoring.metricsMiddleware);
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParse());
+
+app.get('/metrics', async (_req, res: Response) => {
+   logger.warn('hit metrics endpoint');
+   res.set('Content-Type', register.contentType);
+
+   res.end(await register.metrics());
+});
 
 app.get('/api/v1/feed/health', (req, res) => {
    res.status(200).json({ status: 'OK', srv: 'feed' });
