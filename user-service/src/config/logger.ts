9f4156c3704ac4831b0e08c6173aac1e5a43b20f
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-29 13:00:46 +0530

    chore(env config)

--- user-service/src/config/logger.ts
+++ user-service/src/config/logger.ts
@@ -1,9 +1,10 @@
 import winston from 'winston';
-const { combine, errors, timestamp, colorize, json } = winston.format;
+import { ENV } from './env';
+const { combine, errors, timestamp, json } = winston.format;
 
 export const logger = winston.createLogger({
-   level: 'silly',
-   format: combine(timestamp(), json(), errors({ stack: true }), colorize({ all: true })),
+   level: ENV.NODE_ENV === 'production' ? 'info' : 'silly',
+   format: combine(timestamp(), json(), errors({ stack: true })),
    defaultMeta: { service: 'user-srv' },
    transports: [new winston.transports.Console()],
 });
