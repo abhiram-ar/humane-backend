d676788018de7f126f889de25850a638847f5e01
abhiram-ar <abhiram2001.ars@gmail.com>
2025-08-29 13:27:48 +0530

    chore: logger

--- elasticsearch-proxy/src/config/logger.ts
+++ elasticsearch-proxy/src/config/logger.ts
@@ -22,6 +22,6 @@ const { combine, errors, timestamp, json } = winston.format;
 export const logger = winston.createLogger({
    level: ENV.NODE_ENV === 'production' ? 'info' : 'silly',
    format: combine(timestamp(), json(), errors({ stack: true })),
-   defaultMeta: { service: 'user-srv' },
+   defaultMeta: { service: 'elasticsearch-proxy-srv' },
    transports: [new winston.transports.Console()],
 });
