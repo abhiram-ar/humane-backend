fe6e79eace0fc9276d4c92c97924b7da7df3641f
abhiram-ar <abhiram2001.ars@gmail.com>
2025-09-02 13:23:58 +0530

    donw

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -22,5 +22,5 @@ const start = async () => {
       logger.error('Error while starting user service', { error });
    }
 };
-console.log("hodssdfdfsdfdiasdasdsfsdfsf")
+console.log("hodssdfdfsdfdiasdasdsfsdfsf fdf")
 start();
